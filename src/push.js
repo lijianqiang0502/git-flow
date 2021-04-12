f7800363a760f91d1407512a1edc16e994647ee7
李建强 <lijianqiang.seven@bytedance.com>
2021-04-12 17:58:39 +0800

    1

--- src/push.js
+++ src/push.js
@@ -53,10 +53,10 @@ function push() {
         await gitP.commit(commitMsg);
         // var pullBranch = readlineSync.question('Please enter the pull branch： ');
         // currentBranch = pullBranch === "" ? currentBranch : pullBranch;
-        // await gitP.pull('origin', currentBranch,);
-        await gitP.raw('git pull origin ' + currentBranch, (suc, err) => {
-            console.log(suc, err);
-        });
+        await gitP.pull('origin', currentBranch,);
+        // await gitP.raw('git pull origin ' + currentBranch, (suc, err) => {
+        //     console.log(suc, err);
+        // });
         await gitP.push('origin', currentBranch,);
     }
     quickGit();
