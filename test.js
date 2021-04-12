6e31485eb944a0c6fa0c59694a0ce5b58bccbc7e
李坤浩 <likunhao@bytedance.com>
2021-04-12 18:06:38 +0800

    feat: lkh edit

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 console.log('hhhhh', 12342142);
-console.log('hhhhh', 12342142);
-console.log('hhhhh', 12342142);
-console.log('hhhhh', 12342142);
+console.log('aaaaa', 12342142);
+console.log('bbbbb', 12342142);
+console.log('ccccc', 12342142);
