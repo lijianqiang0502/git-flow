fe688efc118c947a821f7c711bb5106a48b8c760
李建强 <lijianqiang.seven@bytedance.com>
2021-04-12 18:06:56 +0800

    feat: test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 console.log('hhhhh', 12342142);
-console.log('hhhhh', 12342142);
-console.log('hhhhh', 12342142);
-console.log('hhhhh', 12342142);
+console.log('xxx', 12342142);
+console.log('yyyy', 12342142);
+console.log('zzzz', 12342142);
